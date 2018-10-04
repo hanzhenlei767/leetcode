3acd62b9bf755802f5170a4a4902ed47d340b361
hanzhenlei767 <42851990+hanzhenlei767@users.noreply.github.com>
2018-10-04 16:56:43 +0800

    对应剑指offer:55

---
+++ 104. 二叉树的最大深度.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root == None:
+            return 0
+        else:
+            left_depth = self.maxDepth(root.left)
+            right_depth = self.maxDepth(root.right)
+        return max(left_depth+1,right_depth+1)
+        
