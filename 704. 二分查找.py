7a33ad561cb2975fc4375c3f3cecd3d12e87e5f4
hanzhenlei767 <42851990+hanzhenlei767@users.noreply.github.com>
2019-01-28 10:10:22 +0800

    二分查找

---
+++ 704. 二分查找.py
@@ -0,0 +1,21 @@
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if nums == []:
+            return -1
+        left = 0
+        right = len(nums)-1
+
+        while left <= right:
+            mid = (left + right)//2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] > target:
+                right = mid - 1
+            if nums[mid] < target:
+                left = mid + 1
+        return -1
