80d603cb1cbdb3b4cad1b5561a2beecd362b35a9
hanzhenlei767 <42851990+hanzhenlei767@users.noreply.github.com>
2019-08-22 10:28:42 +0800

    动态规划

---
+++ 70. 爬楼梯.py
@@ -0,0 +1,22 @@
+
+#斐波那契数列，动态规划问题
+class Solution(object):
+    path = []
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        dp = [0 for _ in range(n+1)]
+        dp[0] = 0
+        dp[1] = 1
+        dp[2] = 2
+        for i in range(3,n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[-1]
