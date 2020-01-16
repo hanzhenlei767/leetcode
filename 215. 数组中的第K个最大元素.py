50b724bf43eda529e224389cc5a96587c6dd5dee
韩振磊 <hanzhenlei@up366.com>
2020-01-16 17:40:09 +0800

    添加快排思想的topK

--- 215. 数组中的第K个最大元素.py
+++ 215. 数组中的第K个最大元素.py
@@ -1,3 +1,38 @@
+#快排思想
+class Solution(object):
+    def findKthLargest(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def partition(num, low, high):
+			'''降序'''
+            pivot = num[low]
+            while (low < high):
+                while (low < high and num[high] <= pivot):
+                    high -= 1
+                num[low] = num[high]
+                while (low < high and num[low] > pivot):
+                    low += 1
+                num[high] = num[low]
+            num[high] = pivot
+            return high
+ 
+        def findkth(num,low,high,k):   #找到数组里第k个数
+            index=partition(num,low,high)
+            if index==k:
+                return num[index]
+            if index<k:
+                return findkth(num,index+1,high,k)
+            else:
+                return findkth(num,low,index-1,k)
+
+        return findkth(nums,0,len(nums)-1,k-1)
+
+
+
+
 class Solution:
     def findKthLargest(self, nums, k):
         """
@@ -42,4 +77,7 @@ class Solution:
                         largest_list.append(i)
                 largest_list =largest_list[1:]
         return largest_list[0]
+  
+  
+  
   
