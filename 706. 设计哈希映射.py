e8665aa20551ded8cda71aad643ea7355db5135e
韩振磊 <hanzhenlei@up366.com>
2020-01-21 17:26:12 +0800

    哈希映射实现

---
+++ 706. 设计哈希映射.py
@@ -0,0 +1,43 @@
+class MyHashMap(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.d = {}
+
+    def put(self, key, value):
+        """
+        value will always be non-negative.
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        self.d[key] = value
+
+    def get(self, key):
+        """
+        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+        :type key: int
+        :rtype: int
+        """
+        if key in self.d:
+            return self.d[key]
+        else:
+            return -1
+
+    def remove(self, key):
+        """
+        Removes the mapping of the specified value key if this map contains a mapping for the key
+        :type key: int
+        :rtype: None
+        """
+        if key in self.d:
+            del self.d[key]
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
