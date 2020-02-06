20d32e04594dc73157229afbda18c27af0ddf942
韩振磊 <hanzhenlei@up366.com>
2020-02-06 20:44:20 +0800

    相同的树

---
+++ 100. 相同的树.py
@@ -0,0 +1,94 @@
+'''
+给定两个二叉树，编写一个函数来检验它们是否相同。
+
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+示例 1:
+
+输入:       1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+输出: true
+示例 2:
+
+输入:      1          1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+输出: false
+示例 3:
+
+输入:       1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+输出: false
+
+'''
+#递归
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p == None and q == None:
+            return True
+        elif p == None or q == None:
+            return False
+        elif p.val == q.val:
+            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        else:
+            return False
+            
+#前序遍历
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        def fun(root):
+            ret,stack = [],[root]
+            while stack:
+                temp = stack.pop()
+                if temp:
+                    ret.append(str(temp.val))
+                    stack.append(temp.right)
+                    stack.append(temp.left)
+                else:
+                    ret.append("#")
+            return ret
+        a = fun(p)
+        b = fun(q)
+        if a == b:
+            return True
+        else:
+            return False
+
+
+        
+
