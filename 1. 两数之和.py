41a059050b268a3889e3c06429ea9f07579d3b85
韩振磊 <hanzhenlei@up366.com>
2020-04-09 10:38:33 +0800

    阿里两数之和

--- 1. 两数之和.py
+++ 1. 两数之和.py
@@ -11,6 +11,8 @@
 所以返回 [0, 1]
 '''
 
+#哈希法-空间换时间
+
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -24,3 +26,46 @@ class Solution(object):
                 return [sub[nums[i]],i]
             else:
                 sub[target - nums[i]] = i
+
+#不使用额外空间
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            dif = target - nums[i]    # 计算差值
+            nums[i] = '#'
+            if dif in nums:           # 查看差值是否在剩余数字中
+                return [i, nums.index(dif)]
+        return []
+
+# 无序数组
+# 数组中两数之和等于target则返回两个数
+# 数组中不存在两数之和等于target则返回两个最接近target的数
+
+
+def twoSum(nums, target):
+    nums.sort()
+    print(nums)
+    left = 0
+    right = len(nums)-1
+    min_diff = float("inf")
+    
+    while left<right:
+        if (nums[left] + nums[right])  == target:
+            return nums[left],nums[right]
+        elif (nums[left] + nums[right])  > target:
+            if abs(nums[left] + nums[right] - target) < min_diff:
+                min_diff = abs(nums[left] + nums[right] - target)
+                last = nums[left],nums[right]
+            right = right - 1
+        else:
+            if abs(nums[left] + nums[right] - target) < min_diff:
+                min_diff = abs(nums[left] + nums[right] - target)
+                last = nums[left],nums[right]
+            left = left + 1
+    return last
