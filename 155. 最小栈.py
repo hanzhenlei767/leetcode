61a83bd5f1612fb80c87ef8994ff08a5920ce191
韩振磊 <hanzhenlei@up366.com>
2020-07-03 09:28:58 +0800

    最小栈

---
+++ 155. 最小栈.py
@@ -0,0 +1,76 @@
+'''
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) —— 将元素 x 推入栈中。
+pop() —— 删除栈顶的元素。
+top() —— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+ 
+
+示例:
+
+输入：
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+输出：
+[null,null,null,null,-3,null,0,-2]
+
+解释：
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+ 
+
+提示：
+
+pop、top 和 getMin 操作总是在 非空栈 上调用。
+
+'''
+
+class MinStack(object):
+
+    def __init__(self):
+        # 数据栈
+        self.data = []
+        # 辅助栈
+        self.helper = []
+
+    def push(self, x):
+        self.data.append(x)
+        if len(self.helper) == 0 or x <= self.helper[-1]:
+            self.helper.append(x)
+        else:
+            self.helper.append(self.helper[-1])
+
+    def pop(self):
+        if self.data:
+            self.helper.pop()
+            return self.data.pop()
+
+    def top(self):
+        if self.data:
+            return self.data[-1]
+
+    def getMin(self):
+        if self.helper:
+            return self.helper[-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
